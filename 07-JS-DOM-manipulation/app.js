b0be7b4a07c99dded3020209274d764bb42eca72
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-01-04 20:36:31 +0500

    Add js dom manipulations

---
+++ 07-JS-DOM-manipulation/app.js
@@ -0,0 +1,21 @@
+// DOM manipulations
+
+// getElementById
+const title = document.getElementById('main-heading');
+console.log(': ', title);
+
+// getElementByClassName
+const listItems = document.getElementsByClassName('list-items');
+console.log(': ', listItems);
+
+// getElementsByTagName
+const listItems1 = document.getElementsByTagName('li');
+console.log(': ', listItems1);
+
+// querySelector
+const container = document.querySelector('div');
+console.log(': ', container);
+
+// querySelectorAll
+const lists = document.querySelectorAll('li');
+console.log('querySelectorAll: ', lists); 
