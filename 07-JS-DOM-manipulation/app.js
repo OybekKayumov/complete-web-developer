08a685b3fca483048d2249d4dd52f5c9804fa407
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-01-05 20:50:45 +0500

    Add js-dom

--- 07-JS-DOM-manipulation/app.js
+++ 07-JS-DOM-manipulation/app.js
@@ -1,5 +1,5 @@
 // DOM manipulations
-
+/*
 // getElementById
 const title = document.getElementById('main-heading');
 console.log(': ', title);
@@ -19,3 +19,38 @@ console.log(': ', container);
 // querySelectorAll
 const lists = document.querySelectorAll('li');
 console.log('querySelectorAll: ', lists); 
+*/
+
+//todo: Styling elements
+// const title = document.querySelector('#main-heading');
+// title.style.color = 'red';
+// console.log(title );
+
+/*
+const listItems = document.querySelectorAll('.list-items');
+for (let i = 0; i < listItems.length; i++) {
+  listItems[i].style.fontSize = '2rem';  
+}
+console.log(listItems); 
+*/
+
+//todo: Creating elements
+const ul = document.querySelector('ul');
+const li = document.createElement('li');
+
+// adding elements
+ul.append(li);
+
+// modifying the text
+const firstListItem = document.querySelector('.list-items');
+
+console.log(firstListItem.innerText);
+// Neo The Matrix
+
+console.log(firstListItem.textContent);
+// Neo
+// The Matrix
+
+console.log(firstListItem.innerHTML);
+{/* <span>Neo</span>
+The Matrix */}
