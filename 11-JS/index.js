e96051746b979db05136855a6a10bb018fa4dc56
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-01-07 06:42:59 +0500

    Add JS conditional

--- 11-JS/index.js
+++ 11-JS/index.js
@@ -136,3 +136,27 @@ if (!(name === "Bob")) {
 }
 // Hi, Bob
 
+// todo:
+// Make a keyless car!
+// This car will only let you drive if you are over 18. Make it do the following:
+
+//using prompt() and alert(), ask a user for their age.
+// IF they say they are below 18, respond with:
+// "Sorry, you are too young to drive this car. Powering off"
+
+// IF they say they are 18, respond with:
+// "Congratulations on your first year of driving. Enjoy the ride!"
+
+// IF they say they are over 18, respond with:
+// "Powering On. Enjoy the ride!"
+
+// car
+let age = prompt("Enter your age: ");
+if (+age < 18 ) {
+  console.log("Sorry, you are too young to drive this car. Powering off");
+} else if (+age > 18) {
+  console.log("Powering On. Enjoy the ride!");
+} else {
+  console.log("Congratulations on your first year of driving. Enjoy the ride!");
+}
+
