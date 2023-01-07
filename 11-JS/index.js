96589d18a26dc4d32538c3068029b37100226fa6
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-01-07 08:12:29 +0500

    Add JS functions

--- 11-JS/index.js
+++ 11-JS/index.js
@@ -201,3 +201,44 @@ console.log(multiply(5, 10));
 // todo: parameters and arguments
 multiply(5,6);  // 5 and 6 are arguments
 // parameters are what a and b is
+
+//
+// Make a keyless car EVEN BETTER!
+// We are improving our car from previous exercise now.
+// Solutions to future exercises will be in the .js file (meaning Javascript file) from now on.
+/*
+function checkDriverAge(age) {  
+  var age = prompt("What is your age?");  
+  if (Number(age) < 18) {
+    console.log("Sorry, you are too young to drive this car. Powering off");
+  } else if (Number(age) > 18) {
+    console.log("Powering On. Enjoy the ride!");
+  } else if (Number(age) === 18) {
+    console.log("Congratulations on your first year of driving. Enjoy the ride!");
+  }
+}
+checkDriverAge();
+*/
+
+//1. Make the above code have a function called checkDriverAge(). Whenever you call this function, you will get prompted for age. Use Function Declaration to create this function.
+// Notice the benefit in having checkDriverAge() instead of copying and pasting the function every time?
+
+//2. Create another function that does the same thing, called checkDriverAge2() using Function Expression.
+
+//BONUS: Instead of using the prompt. Now, only use console.log and make the checkDriverAge() function accept an argument of age, so that if you enter:
+// checkDriverAge(92);
+// it returns "Powering On. Enjoy the ride!"
+
+const checkDriverAge2 = function() {  
+  var age = prompt("What is your age?");
+  if (Number(age) < 18) {
+    return "Sorry, you are too young to drive this car. Powering off";
+  } else if (Number(age) > 18) {
+    return "Powering On. Enjoy the ride!";
+  } else if (Number(age) === 18) {
+    return "Congratulations on your first year of driving. Enjoy the ride!";
+  }
+}
+
+// console.log(checkDriverAge2(29));
+console.log( checkDriverAge2());
