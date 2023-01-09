8123337c573e6d3c65ccf931f67cb3d8f3b46801
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-01-09 12:55:45 +0500

    Add js bg generator

--- 12-1-bg-generator/script.js
+++ 12-1-bg-generator/script.js
@@ -1,4 +1,19 @@
 const css = document.querySelector('h3');
 const color1 = document.querySelector('.color1');
 const color2 = document.querySelector('.color2');
-console.log(css, color1, color2);
+const body = document.getElementById('gradient');
+// console.log(css, color1, color2, body);
+// body.style.background = 'green';
+
+function setGradient() {
+  body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`
+}
+
+
+color1.addEventListener('input', function () {
+  console.log(color1.value);
+  setGradient();    // ! ()
+})
+
+color2.addEventListener('input', setGradient)   // ! ()
+
