8a12adfeda8446177990f53cd31b58e0c7d59687
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-01-12 15:22:36 +0500

    Add react card

--- 19-react.js/robofriends/src/App.js
+++ 19-react.js/robofriends/src/App.js
@@ -1,15 +1,32 @@
-import React, { Component} from 'react';
-import Hello from './Hello';
+// import React, { Component} from 'react';
+import Hello from './components/Hello';
 import './App.css';
+import Card from './components/Card';
 
+const App = () => {  
+  return (
+    <div className="App">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+
+}
+
+/*
 class App extends Component{
   render () {
     return (
       <div className="App">
-        <Hello greeting={'Hello' + ' React Ninja'} />
+        <Card />
       </div>
     );
   }
 }
+*/
 
 export default App;
+
+{/* <Hello greeting={'Hello' + ' React Ninja'} /> */}
---
+++ 19-react.js/robofriends/src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img src="https://robohash.org/test?200x200" alt="robots" />
+      <div>
+        <h2>Jane Doe</h2>
+        <p>j.doe@t-gmail.com</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card;
