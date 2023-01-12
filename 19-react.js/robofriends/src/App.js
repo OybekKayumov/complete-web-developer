a552b3642d9e89b78696b906af7758451466b35a
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-01-12 14:36:08 +0500

    Add react component

--- 19-react.js/robofriends/src/App.js
+++ 19-react.js/robofriends/src/App.js
@@ -1,11 +1,15 @@
+import React, { Component} from 'react';
+import Hello from './Hello';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+class App extends Component{
+  render () {
+    return (
+      <div className="App">
+        <Hello />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ 19-react.js/robofriends/src/Hello.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import './Hello.css';
+
+class Hello extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+        <p>Welcome to React</p>
+      </div>
+    )
+  }
+}
+
+export default Hello;
