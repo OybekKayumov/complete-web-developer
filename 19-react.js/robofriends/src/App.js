0d5182f91d293baedc952317c2099716c42870f7
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-01-12 16:50:42 +0500

    Add react cardlist component

--- 19-react.js/robofriends/src/App.js
+++ 19-react.js/robofriends/src/App.js
@@ -3,30 +3,12 @@ import Hello from './components/Hello';
 import './App.css';
 import Card from './components/Card';
 import { robots } from './components/robots';
+import CardList from './components/CardList';
 
 const App = () => {  
   return (
     <div className="App">
-      <Card 
-        id={robots[0].id}
-        name={robots[0].name}
-        email={robots[0].email}
-      />
-      <Card 
-        id={robots[1].id}
-        name={robots[1].name}
-        email={robots[1].email}
-      />
-       <Card 
-        id={robots[2].id}
-        name={robots[2].name}
-        email={robots[2].email}
-      />
-       <Card 
-        id={robots[3].id}
-        name={robots[3].name}
-        email={robots[3].email}
-      />
+      <CardList robots={robots} />
     </div>
   );
 
---
+++ 19-react.js/robofriends/src/components/CardList.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({ robots }) => {
+  return (
+    <>
+    <Card 
+      id={robots[0].id}
+      name={robots[0].name}
+      email={robots[0].email}
+      />
+    <Card 
+      id={robots[1].id}
+      name={robots[1].name}
+      email={robots[1].email}
+      />
+      <Card 
+      id={robots[2].id}
+      name={robots[2].name}
+      email={robots[2].email}
+      />
+      <Card 
+      id={robots[3].id}
+      name={robots[3].name}
+      email={robots[3].email}
+      />
+    </>
+  )
+}
+
+export default CardList;
