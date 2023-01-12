85b6d2a0e58f78b632d9cbd3e35feb65821adb5a
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-01-12 15:39:00 +0500

    Add react props destructuring 2

--- 19-react.js/robofriends/src/components/Card.jsx
+++ 19-react.js/robofriends/src/components/Card.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
-const Card = (props) => {
-  const { name, email, id } = props;
+// const Card = (props) => {
+const Card = ({ name, email, id }) => {
+  // const { name, email, id } = props;
 
   return (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
