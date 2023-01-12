23b884d79079dc3e64463c132c28d2326d9d03ee
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-01-12 15:37:45 +0500

    Add react props destructuring

--- 19-react.js/robofriends/src/App.js
+++ 19-react.js/robofriends/src/App.js
@@ -17,8 +17,16 @@ const App = () => {
         name={robots[1].name}
         email={robots[1].email}
       />
-      <Card />
-      <Card />
+       <Card 
+        id={robots[2].id}
+        name={robots[2].name}
+        email={robots[2].email}
+      />
+       <Card 
+        id={robots[3].id}
+        name={robots[3].name}
+        email={robots[3].email}
+      />
     </div>
   );
 
