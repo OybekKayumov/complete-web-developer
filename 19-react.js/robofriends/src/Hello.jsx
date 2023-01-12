6f622a2c307d50bb63e27f2665a40f55da40f928
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-01-12 14:58:37 +0500

    Add react tachyons

--- 19-react.js/robofriends/src/Hello.jsx
+++ 19-react.js/robofriends/src/Hello.jsx
@@ -4,7 +4,7 @@ import './Hello.css';
 class Hello extends Component {
   render() {
     return (
-      <div>
+      <div className="f1 tc">
         <h1>Hello</h1>
         <p>Welcome to React</p>
       </div>
@@ -13,3 +13,6 @@ class Hello extends Component {
 }
 
 export default Hello;
+
+// .f1 font-size:3rem
+// .tc text-align:center
