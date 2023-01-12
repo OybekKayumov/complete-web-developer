699aca30ada3297ac70bbf28cae6c78baf59b581
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-01-12 15:32:08 +0500

    Add react robots object

--- 19-react.js/robofriends/src/App.js
+++ 19-react.js/robofriends/src/App.js
@@ -2,12 +2,21 @@
 import Hello from './components/Hello';
 import './App.css';
 import Card from './components/Card';
+import { robots } from './components/robots';
 
 const App = () => {  
   return (
     <div className="App">
-      <Card />
-      <Card />
+      <Card 
+        id={robots[0].id}
+        name={robots[0].name}
+        email={robots[0].email}
+      />
+      <Card 
+        id={robots[1].id}
+        name={robots[1].name}
+        email={robots[1].email}
+      />
       <Card />
       <Card />
     </div>
--- 19-react.js/robofriends/src/index.js
+++ 19-react.js/robofriends/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'tachyons';
+import { robots } from './components/robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
