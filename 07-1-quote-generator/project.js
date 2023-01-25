7542236ed1f0c0608a81d701cd2116624358b18e
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-01-25 18:28:25 +0500

    Add css

---
+++ 07-1-quote-generator/project.js
@@ -0,0 +1,8 @@
+// variables
+let btn = document.querySelector('#new-quote');
+let quote = document.querySelector('.quote');
+let person = document.querySelector('.person');
+
+const quotes = [{
+  
+}]
