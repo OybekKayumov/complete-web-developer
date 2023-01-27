180e21200afbd2787f38383aacc47b3ff6cfea19
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-01-27 10:53:51 +0500

    Add timer project 7-5: todo list

--- 07-5-todo-list/project.js
+++ 07-5-todo-list/project.js
@@ -1,1 +1,31 @@
-const 
+const addTask = document.getElementById('add-task');
+const taskContainer = document.getElementById('task-container');
+const inputTask = document.getElementById('input-task');
+
+addTask.addEventListener('click', function () {
+  let task = document.createElement('div');
+  task.classList.add('task');
+
+  let li = document.createElement('li');
+  li.innerText = `${inputTask.value}`;
+  task.appendChild(li);
+
+  let checkBtn = document.createElement('button');
+  checkBtn.innerHTML = `<i class="fa-solid fa-check"></i>`;
+  checkBtn.classList.add('checkTask');
+  task.appendChild(checkBtn);
+
+  let deleteBtn = document.createElement('button');
+  deleteBtn.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
+  deleteBtn.classList.add('deleteTask');
+  task.appendChild(deleteBtn);
+
+  if (inputTask.value === "") {
+    alert("Please enter a task");
+  } else {
+    taskContainer.appendChild(task);
+  }
+
+  inputTask.value = "";
+  
+})
