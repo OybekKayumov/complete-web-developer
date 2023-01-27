db56e8cb3757dbda04a93c99eebbf2c7ec31c803
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-01-27 08:58:24 +0500

    Add timer project 7-4

--- 07-4-timer/project.js
+++ 07-4-timer/project.js
@@ -1,1 +1,26 @@
-// const 
+const startStopBtn = document.querySelector('#startStopBtn');
+const resetBtn =  document.querySelector('#resetBtn');
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+function stopWatch() {
+  seconds ++;
+
+  if(seconds/ 60 === 1) {
+    seconds = 0;
+    minutes ++
+
+    if (minutes / 60 === 1) {
+      minutes = 0;
+      hours ++;
+    }
+  }
+
+  let displayTimer = document.getElementById('timer').innerText = 
+    hours + ':' + minutes + ':' + seconds;
+}
+
+window.setInterval(stopWatch, 1000)
+
