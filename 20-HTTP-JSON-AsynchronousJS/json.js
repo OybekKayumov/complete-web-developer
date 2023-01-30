989b3528d8d8470814a8c12fa4da5aa552a409a5
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-01-30 15:06:51 +0500

    Add promises exercises

--- 20-HTTP-JSON-AsynchronousJS/json.js
+++ 20-HTTP-JSON-AsynchronousJS/json.js
@@ -105,3 +105,50 @@ Promise.all(urls.map(url => {
 }).catch(() => console.log('error: ' ))
 
 // output
+
+//TODO:
+// Solve the questions below:
+
+// #1) Create a promise that resolves in 4 seconds and returns "success" string
+const promiseN1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('success')
+  }, 4000)    
+})
+
+// #2) Run the above promise and make it console.log "success"
+promiseN1.then(console.log)
+// or
+promiseN1.then(resp => console.log(resp))
+
+// #3) Read about Promise.resolve() and Promise.reject(). How can you make
+// the above promise shorter with Promise.resolve() and console login "success"
+const promiseN2 = Promise.resolve(
+  setTimeout(() => {
+    console.log('success');
+  }, 4000)
+)
+
+// #4) Catch this error and console log 'Ooops something went wrong'
+// Promise.reject('failed').catch(console.log('Ooops something went wrong'))
+
+// #5) Use Promise.all to fetch all of these people from Star Wars (SWAPI) at the same time.
+// Console.log the output and make sure it has a catch block as well.
+const urls2 = [
+  'http://swapi.dev/api/people/1',
+  'http://swapi.dev/api/people/2',
+  'http://swapi.dev/api/people/3',
+  'http://swapi.dev/api/people/4'
+]
+
+Promise.all(urls2.map(
+  url => fetch(url).then(people => people.json())
+)).then(arr => {
+  console.log('1: ', arr[0]);
+  console.log('2: ', arr[1]);
+  console.log('3: ', arr[2]);
+  console.log('4: ', arr[3]);
+}).catch(err => console.log('fix it!: ', err ))
+
+// #6) Change one of your urls above to make it incorrect and fail the promise
+// does your catch block handle it?
