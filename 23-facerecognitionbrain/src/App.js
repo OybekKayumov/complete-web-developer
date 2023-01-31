c663142a316cf78583c9d298f267d196fe0c7083
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-01-31 11:38:23 +0500

    Add face-recognition-app

---
+++ 23-facerecognitionbrain/src/App.js
@@ -0,0 +1,16 @@
+import Navigation from './components/Navigation/Navigation';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Navigation />
+{/*      
+     <Logo />
+     <ImageLinkForm />
+     <FaceRecognition /> */}
+    </div>
+  );
+}
+
+export default App;
