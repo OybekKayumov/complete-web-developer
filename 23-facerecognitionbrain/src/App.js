319cfc5a748116eb9d4618fbb3a43b80003f4e40
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-01-31 17:00:17 +0500

    Add props

--- 23-facerecognitionbrain/src/App.js
+++ 23-facerecognitionbrain/src/App.js
@@ -2,17 +2,37 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Rank from './components/Rank/Rank';
 import './App.css';
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import { Component, useCallback } from "react";
 
-function App() {
-  return (
-    <div className="App">
-     <Navigation />
-     <Logo />
-     <ImageLinkForm />
-     <FaceRecognition /> 
-    </div>
-  );
+// function App() {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  onInputChange = (e) => {
+    console.log('e: ', e.target.value);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles />
+
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} />
+        <FaceRecognition /> 
+      </div>
+    );
+  }
 }
-
 export default App;
