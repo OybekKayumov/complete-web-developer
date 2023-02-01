d0c2db401f32626492c1681f40f8a0c7c4eabb16
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-02-01 18:07:30 +0500

    Add request header, method, url

--- 24-nodejs-expressjs/server.js
+++ 24-nodejs-expressjs/server.js
@@ -1,7 +1,14 @@
 const http = require('http');
+const { url } = require('inspector');
 
-const server = http.createServer(() => {
-  console.log('I hear you! Thanks for the request');
+const server = http.createServer((request, response) => {
+  // console.log('I hear you! Thanks for the request');
+  console.log("headers ", request.headers);
+  console.log('method', request.method);
+  console.log('url', request.url);
+
+  response.setHeader('Content-Type', 'text/html');
+  response.end('<h1>Hello</h1>')
 })
 
 server.listen(3000);
