9f15173dca1b6245cc05dbd757702bb98acf0ba2
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-02-01 15:46:20 +0500

    Add module exports

--- 24-nodejs-expressjs/script.js
+++ 24-nodejs-expressjs/script.js
@@ -1,10 +1,12 @@
 // import largeNumber from "./script2"; //! error
-const largeNumber = require('./script2');
+// const largeNumber = require('./script2'); [object Object]
+const script2 = require('./script2.js');
 
-const a = largeNumber;
+// const a = largeNumber; // a+b:  [object Object]5
+const a = script2.largeNumber; // a+b:  361
 const b = 5;
 console.log('a+b: ',a + b);
-// a+b:  [object Object]5
+// a+b:  361
 
 //todo:
 console.log(__dirname );
--- 24-nodejs-expressjs/script2.js
+++ 24-nodejs-expressjs/script2.js
@@ -2,6 +2,6 @@ const largeNumber = 356;
 
 // export default largeNumber;
 // module.exports largeNumber;
-module.export = {
+module.exports = {
   largeNumber: largeNumber
 }
