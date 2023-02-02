6b4c78144c442ac09220da0d023d1246be6012b3
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-02-02 11:19:57 +0500

    Add REST API send status 404

--- 24-nodejs-expressjs/server.js
+++ 24-nodejs-expressjs/server.js
@@ -9,12 +9,13 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 app.get('/:id', (req, res) => {  
-  // console.log( req.query )
-  
+  // console.log( req.query )  
   // req.body
   // console.log( req.headers )
+  // res.send('getting root')
+
   console.log( req.params )
-  res.send('getting root')
+  res.status(404).send('not found!')
 })
 
 app.listen(3000);
