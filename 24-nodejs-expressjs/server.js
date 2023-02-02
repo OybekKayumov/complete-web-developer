57b13c41f14736e9289f262021d299dc327f74b2
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-02-02 10:52:55 +0500

    Add post new user request

--- 24-nodejs-expressjs/server.js
+++ 24-nodejs-expressjs/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
+
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json())
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 app.get('/', (req, res) => {  
   res.send('getting root')
@@ -10,10 +16,16 @@ app.get('/profile', (req, res) => {
 })
 app.post('/profile', (req, res) => {  
   console.log(req.body );
-  const user = {
-    name: 'Player',
-    hobby: 'soccer'
-  }
-  res.send(user)
+  
+  res.send('Success!')
 })
 app.listen(3000);
+
+
+// const user = {
+//   name: 'Player',
+//   hobby: 'soccer'
+// }
+// res.send(user)
+// app.use(express.urlencoded({extended: false}));
+// app.use(express.json());
