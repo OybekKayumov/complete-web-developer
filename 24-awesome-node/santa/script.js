7bd4aab8cc81901f94c7f72e8936ace283ff56b9
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-02-02 16:17:00 +0500

    Add santa exercise

--- 24-awesome-node/santa/script.js
+++ 24-awesome-node/santa/script.js
@@ -4,6 +4,8 @@ const fs = require('fs')
 
 function question1() {
   fs.readFile('./santa.txt', (err, data) => {
+    console.time('santa-time'); //!
+
     const direction = data.toString();
     console.log('direction: ', direction);
 
@@ -17,8 +19,32 @@ function question1() {
         return acc -= 1;
       }
     }, 0)
+
+    console.timeEnd('santa-time'); //!
     console.log('answer, floor: ', answer);
   })
 }
 
-question1();
+// question1();
+
+// 2 when does Santa first enter the basement
+function question2() {
+  fs.readFile('./santa.txt', (err, data) => {
+    const direction = data.toString();
+    const directionArr = direction.split('');
+    let acc = 0;
+    let counter = 0;
+    const answer = directionArr.some((curr) => {
+      if (curr === '(') {
+        acc += 1;
+      } else if (curr === ')') {
+        acc -= 1;
+      }
+      counter ++;
+      return acc < 0;
+    })
+    console.log('basement entered at: ', counter);
+  })
+}
+
+question2();
