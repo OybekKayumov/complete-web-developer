e7e288679a2a1aeaeb6072bacc8f3d5cecadf15e
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-02-02 18:27:34 +0500

    Add back-end server

--- 25-SmartBrainBackEnd-Server/server.js
+++ 25-SmartBrainBackEnd-Server/server.js
@@ -2,12 +2,34 @@ const express = require('express');
 
 const app = express();
 
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: '124',
+      name: 'Sally',
+      email: 'sally@gmail.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+}
+
 app.get('/', (req, res) => {
   res.send('this is working')
 })
 
 app.post('/signin', (req, res) => {
-  res.send('signing')
+  // res.send('signing')
+  res.json('signing')
 })
 
 app.listen(3000, () => {
