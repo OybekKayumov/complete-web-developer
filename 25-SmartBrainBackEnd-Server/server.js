6ac1da439c0be31237960e694524878423625b61
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-02-02 19:38:19 +0500

    Add post users

--- 25-SmartBrainBackEnd-Server/server.js
+++ 25-SmartBrainBackEnd-Server/server.js
@@ -42,6 +42,22 @@ app.post('/signin', (req, res) => {
   }
 })
 
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push(
+    {
+      id: '125',
+      name: name,
+      email: email,
+      password: password,
+      entries: 0,
+      joined: new Date()
+    }
+  )
+
+  res.json(database.users[database.users.length-1])
+})
+
 app.listen(3000, () => {
   console.log('app is running on port 3000...');
 })
