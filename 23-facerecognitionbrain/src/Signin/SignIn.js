26e3162194b1a9a51cde80df00d28f84d778ef47
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-02-03 17:01:53 +0500

    Add input onChange fns

--- 23-facerecognitionbrain/src/Signin/SignIn.js
+++ 23-facerecognitionbrain/src/Signin/SignIn.js
@@ -19,6 +19,12 @@ class SignIn extends Component {   // ({ onRouteChanges })
     this.setState({signInPwd: e.target.value})
   }
 
+  // fetch data from input
+  onSubmitSignIn = () => {
+    console.log(this.state);
+    this.props.onRouteChanges('home');
+  }
+
   render () {
     // destructuring
     const { onRouteChanges } = this.props;
@@ -31,11 +37,23 @@ class SignIn extends Component {   // ({ onRouteChanges })
               <legend className="f1 fw6 ph0 mh0">Sign In</legend>
               <div className="mt3">
                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
+                <input 
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                  type="email" 
+                  name="email-address"  
+                  id="email-address" 
+                  onChange={this.onEmailChange}
+                />
               </div>
               <div className="mv3">
                 <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+                <input 
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                  type="password" 
+                  name="password"  
+                  id="password" 
+                  onChange={this.onPasswordChange}
+                />
               </div>
             </fieldset>
             <div className="">
@@ -44,7 +62,8 @@ class SignIn extends Component {   // ({ onRouteChanges })
                 type="submit" 
                 value="Sign in" 
                 // onClick={() => this.props.onRouteChanges('home')} // run when onClick happens
-                onClick={() => onRouteChanges('home')} 
+                // onClick={() => onRouteChanges('home')} 
+                onClick={this.onSubmitSignIn} 
               />
             </div>
             <div className="lh-copy mt3">
