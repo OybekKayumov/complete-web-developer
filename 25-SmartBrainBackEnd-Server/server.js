c57dca12e52c74d385c4c9d52c06fc453f0bab7f
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-02-05 15:45:28 +0500

    Add register module

--- 25-SmartBrainBackEnd-Server/server.js
+++ 25-SmartBrainBackEnd-Server/server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 
 // knexjs import and run
 const knex = require('knex');
+const register = require('./controllers/register');
 
 const db_postgres = knex({
   client: 'pg',
@@ -92,34 +93,7 @@ app.post('/signin', (req, res) => {
 })
 
 // register
-app.post('/register', (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-
-  db_postgres.transaction(trx => {
-    trx.insert({
-      hash: hash,
-      email: email
-    })
-    .into('login')
-    .returning('email')
-    .then(loginEmail => {
-      return trx('users')
-        .returning('*') 
-        .insert({
-          email: loginEmail[0].email,
-          name: name, 
-          joined: new Date()
-        })
-        .then(user => {
-          res.json(user[0])
-        })
-    })
-    .then(trx.commit)
-    .catch(trx.rollback)
-  })  
-  .catch(err => res.status(400).json('unable to register...'))
-})
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)} )
 
 // profile
 app.get('/profile/:id', (req, res) => {
