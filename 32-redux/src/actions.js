81dafb6f6bef90a6f30cd76e3838319fe34157de
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-02-06 17:22:33 +0500

    Add reducer

--- 32-redux/src/actions.js
+++ 32-redux/src/actions.js
@@ -16,6 +16,7 @@ export const setSearchField = (text) => ({
 
 export const requestRobots = dispatch => {
   dispatch({ type: REQUEST_ROBOTS_PENDING})
+  
   fetch('https://jsonplaceholder.typicode.com/users')
   .then(response => response.json())
   .then(data => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data}))
