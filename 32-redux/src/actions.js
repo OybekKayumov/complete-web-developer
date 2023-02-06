50aab676ac450eee5d6aeb0e0b2642b4e5ff4015
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-02-06 14:43:49 +0500

    Add reducer

--- 32-redux/src/actions.js
+++ 32-redux/src/actions.js
@@ -1,9 +1,11 @@
+import { CHANGE_SEARCH_FIELD } from "./constants";
+
 // action name is setSearchField, it is going to take (text), which is a user input and going to return an object
 // object with type setSearchField, action 'CHANGE_SEARCH_FIELD' and going to send (text) - whatever data is needed to go on to the reducer 
 // constant - capitalized
 
 export const setSearchField = (text) => ({
-  type: 'CHANGE_SEARCH_FIELD',
+  type: CHANGE_SEARCH_FIELD,
   payload: text
 })
 
