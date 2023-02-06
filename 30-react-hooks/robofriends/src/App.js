ea079a80e6a3f6a9ec3b816830c6a0ed3e121230
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-02-06 12:04:57 +0500

    Add react hooks

--- 30-react-hooks/robofriends/src/App.js
+++ 30-react-hooks/robofriends/src/App.js
@@ -1,89 +1,57 @@
 import React, { useState, useEffect } from 'react';
-// import Hello from './components/Hello';
-// import Card from './components/Card';
 import CardList from './components/CardList';
-// import { robots } from './components/robots';
 import './App.css';
 import SearchBox from './components/SearchBox';
 import Scroll from './components/Scroll';
 import ErrorBoundry from './components/ErrorBoundry';
 
 function App () {
-  // constructor() {
-  //   super()
-  //   this.state = {
-  //     robots: [],
-  //     searchField: ''
-  //   }
-
-  //   console.log('1- constructor');
-  // }
-
+  
   // states with initial state, array destructuring
   const [robots, setRobots] = useState([]);
   const [searchField, setSearchField] = useState('');
+  const [count, setCount] = useState(0);
 
-  // lifecycle hooks, useEffect
-
-
-  // componentDidMount() {
-  //   fetch('https://jsonplaceholder.typicode.com/users')
-  //     .then(response => {
-  //       return response.json();
-  //     })
-  //     .then(users => {
-  //       this.setState({ robots: users})
-  //     })
-  //   // this.setState({ robots: robots })
-  //   console.log('2- componentDidMount');
-  // }
+  // lifecycle hooks, useEffect  
   //! side effect, and this will auto run every time with App function
   useEffect(() => {  
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => {setRobots(users)})
-  }, [])  // second parameter, avoid rerendering 
+    
+    console.log(count)
+  }, [count])  // [], second parameter, avoid rerendering 
   // optional list, run if only changed
   // [searchField] --> if searchField changes, run useEffect 
-  // [] 
+  //! [count] -- only runs if count changes
 
-  const onSearchChange = (e) => { // use arrow fn, no this keyword, no error
+  const onSearchChange = (e) => { 
     setSearchField(e.target.value)
   }
 
-    const filteredRobot = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchField.toLowerCase())
-    })
+  const filteredRobot = robots.filter(robot => {
+    return robot.name.toLowerCase().includes(searchField.toLowerCase())
+  })
 
-    console.log('3- render', robots, searchField);
+  console.log('3- render', robots, searchField);
 
-    // loading
-    // if (robots.length === 0) {
-    if (!robots.length) {
-      return <h1>Loading...</h1>
-    } else {
-        return (
-          <div className="App tc">
-            <h1 className='f1'>RoboFriends</h1>
-            <SearchBox searchChange={onSearchChange} />
-            <Scroll>
-              <ErrorBoundry>
-                <CardList robots={filteredRobot} />
-              </ErrorBoundry>
-            </Scroll>
-          </div>
-        );
-    }
+  // loading
+  if (!robots.length) { // if (robots.length === 0) {
+    return <h1>Loading...</h1>
+  } else {
+      return (
+        <div className="App tc">
+          <h1 className='f1'>RoboFriends</h1>
+          <button onClick={() => setCount(count +1)}>Click Me</button>
+          <SearchBox searchChange={onSearchChange} />
+          <Scroll>
+            <ErrorBoundry>
+              <CardList robots={filteredRobot} />
+            </ErrorBoundry>
+          </Scroll>
+        </div>
+      );
   }
-
+}
 
 export default App;
-
-// robots: robots,
-
-// https://reactjs.org/docs/react-component.html
-// ! lifecycle: renders again
-// 1- constructor
-// 3- render
-// 2- componentDidMount
-// 3- render
