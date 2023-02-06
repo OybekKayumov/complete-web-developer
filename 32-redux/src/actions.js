39cc3aee41d4486b2c62a7de2001be164394e46b
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-02-06 14:29:58 +0500

    Add action

---
+++ 32-redux/src/actions.js
@@ -0,0 +1,11 @@
+// action name is setSearchField, it is going to take (text), which is a user input and going to return an object
+// object with type setSearchField, action 'CHANGE_SEARCH_FIELD' and going to send (text) - whatever data is needed to go on to the reducer 
+// constant - capitalized
+
+export const setSearchField = (text) => ({
+  type: 'CHANGE_SEARCH_FIELD',
+  payload: text
+})
+
+
+
