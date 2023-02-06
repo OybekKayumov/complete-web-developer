690090df4549db6c08cce4fddda63789e6229259
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-02-06 17:13:21 +0500

    Add redux-thunk

--- 32-redux/src/actions.js
+++ 32-redux/src/actions.js
@@ -1,4 +1,9 @@
-import { CHANGE_SEARCH_FIELD } from "./constants";
+import { 
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED 
+} from "./constants";
 
 // action name is setSearchField, it is going to take (text), which is a user input and going to return an object
 // object with type setSearchField, action 'CHANGE_SEARCH_FIELD' and going to send (text) - whatever data is needed to go on to the reducer 
@@ -8,6 +13,14 @@ export const setSearchField = (text) => ({
   type: CHANGE_SEARCH_FIELD,
   payload: text
 })
+
+export const requestRobots = dispatch => {
+  dispatch({ type: REQUEST_ROBOTS_PENDING})
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(data => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data}))
+  .catch(err => dispatch({ type: REQUEST_ROBOTS_FAILED, payload: err}))
+}
 
 
 
--- 32-redux/src/index.js
+++ 32-redux/src/index.js
@@ -3,16 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { Provider, connect } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './App';
 import 'tachyons';
 import { robots } from './components/robots';
 import { searchRobots } from './reducers';
 
-// middleware
+// middleware, log actions
 const logger = createLogger();
 
-const store = createStore(searchRobots, applyMiddleware(logger));
+const store = 
+  createStore(searchRobots, applyMiddleware(thunkMiddleware, logger));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
